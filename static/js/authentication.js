72d5cdb67b2aca0abca752cdd46643a7eb8ade03
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-05-05 22:59:25 +0000

    fix authentications.html
    get skipta á milli login og signup síðu
    
    refactor authentication -- create authentication.js in static
    
    etc

---
+++ static/js/authentication.js
@@ -0,0 +1,9 @@
+function displaySignup() {
+    document.getElementById('login-container').style.display = 'none';
+    document.getElementById('signup-container').style.display = 'flex';
+}
+
+function displayLogin() {
+    document.getElementById('signup-container').style.display = 'none';
+    document.getElementById('login-container').style.display = 'flex';
+}
