a9b4f8bafb92f7f629ff774a404273464f1ac515
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-05-08 03:08:43 +0000

    OFFER database connected

--- offer/models.py
+++ offer/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 
+class States(models.TextChoices):
+    PENDING = 'PEND', 'Pending'
+    ACCEPTED = 'ACCE', 'Accepted'
+    REJECTED = 'REJ', 'Rejected'
+    CONTINGENT = 'CONT', 'Contingent'
+
 class Offer(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=20)
     expiration_date = models.DateField()
-    state = models.TextField()
+    state = models.CharField(
+        max_length=4,
+        choices=States.choices,
+        default=States.PENDING
+    )
     property_id = models.IntegerField()
     seller = models.IntegerField()
     image = models.ImageField()
+
+    def __str__(self):
+        return f"{self.property_id} {self.price} ({self.id})"
