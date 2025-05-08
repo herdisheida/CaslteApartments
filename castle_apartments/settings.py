8181144be9fa7b0130bae727bcda3653a0375632
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-05-08 09:49:35 +0000

    DELTE DATABASE

--- castle_apartments/settings.py
+++ castle_apartments/settings.py
@@ -127,5 +127,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
---
+++ offer/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+import offer.models
+
+# admin.site.register(offer.models.Offer)
