56ebb8319cd1d04fd8e2b45a19794ab25a17e778
LAPTOP-PHSLA1FS\julia <juliagunnlaugs05@gmail.com>
2025-05-12 13:41:22 +0000

    little better pfp

--- user_profile/forms/profile_form.py
+++ user_profile/forms/profile_form.py
@@ -1,6 +1,22 @@
 from user_profile.models import UserProfile
 from django.forms import ModelForm
 from django import forms
+from user_profile import models
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['name', 'image']
+        widgets = {
+            'image': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+                'accept': 'image/*'
+            }),
+            'name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+        }
+
 
 class ProfileForm(ModelForm):
     class Meta:
