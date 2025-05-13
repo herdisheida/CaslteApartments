308e0f3b18d9f7d2089b3e837137aa54904429b1
LAPTOP-PHSLA1FS\julia <juliagunnlaugs05@gmail.com>
2025-05-13 12:17:30 +0000

    SENDING OFFER WORKS

--- offer/views.py
+++ offer/views.py
@@ -2,6 +2,7 @@
 from offer.models import Offer
 from django.shortcuts import render, redirect, get_object_or_404
 from property.models import Property
+from user_profile.models import SellerProfile
 from .forms import OfferForm
 # Create your views here.
 
@@ -17,11 +18,15 @@ def payment(request):
 
 def submit_offer_prop(request, property_id):
     property_obj = get_object_or_404(Property, id=property_id)
+    seller = property_obj.seller
+    buyer_profile = request.user.userprofile
 
     if request.method == 'POST':
         form = OfferForm(request.POST)
         if form.is_valid():
             offer = form.save(commit=False)
+            offer.seller = seller
+            offer.buyer = buyer_profile
             offer.property = property_obj
             offer.save()
             return redirect('payment-index')  # or any success page
