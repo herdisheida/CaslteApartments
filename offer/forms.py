b082aca385521a558691df4006869bde5c0133e6
LAPTOP-PHSLA1FS\julia <juliagunnlaugs05@gmail.com>
2025-05-13 12:26:53 +0000

    SENDING OFFER WORKS

---
+++ offer/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Offer
+
+class OfferForm(forms.ModelForm):
+    class Meta:
+        model = Offer
+        # fields = ['price', 'expiration_date']
+        exclude = ('state', 'seller', 'property', 'buyer')
+        widgets = {
+            'expiration_date': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'},
+                format='%Y-%m-%d'
+            ),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['expiration_date'].input_formats = ['%Y-%m-%d']
