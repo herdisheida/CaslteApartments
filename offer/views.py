e4fb6f99ef090b6ea0aef5c1d2ffcca92e8e2dc5
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-05-13 11:59:28 +0000

    only see YOUR sumited offers

--- offer/views.py
+++ offer/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from offer.models import Offer
 # Create your views here.
 
-def index(request):
+def display_submitted_offers(request):
     user_offers = Offer.objects.filter(buyer=request.user.userprofile)
     return render(request, 'offer/submitted_offer/offers.html', {
         'offers': user_offers,
+    })
+
+
+def display_received_offers(request):
+    user_seller_profile = request.user.userprofile.sellerprofile
+    offers = Offer.objects.filter(seller=user_seller_profile)
+    return render(request, 'offer/submitted_offer/offers.html', {
+        'received_offers': offers,
     })
 
 def payment(request):
