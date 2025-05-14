f015082f7c115df52d31c7b68172064b218a08f0
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-05-14 17:00:38 +0000

    able to change the profile pic of the users

--- user_profile/views.py
+++ user_profile/views.py
@@ -22,9 +22,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
-from user_profile.forms.profile_form import ProfileForm
+from user_profile.forms import ProfileForm
 from user_profile.models import UserProfile
-from .form import SellerProfileForm
+from .forms import SellerProfileForm
 
 
 def register(request):
@@ -46,23 +46,18 @@ def register(request):
     })
 
 def profile(request):
-    user_profile = UserProfile.objects.filter(user=request.user).first()
+    profile_instance = UserProfile.objects.filter(user=request.user).first()
 
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=user_profile)
+        form = ProfileForm(request.POST, request.FILES, instance=profile_instance)
         if form.is_valid():
-            profile_instance = form.save(commit=False)
-            profile_instance.user = request.user
-
-            if not user_profile.name:
-                profile_instance.name = request.user.username # default profile-name is user-username
-
-            profile_instance.save()
+            form.save()
             return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile_instance)
 
-    return render(request, 'profile/profile.html', {
-        'form': ProfileForm(instance=user_profile),
-    })
+    return render(request, 'profile/profile.html', {'form': form})
+
 
 #
 # def seller_index(request):
