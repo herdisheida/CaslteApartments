5bef866d5067dcdd105770fbdead0d9bfe4446dd
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-05-14 18:45:34 +0000

    localhost:8000 takes you straight to the registeer page

--- castle_apartments/urls.py
+++ castle_apartments/urls.py
@@ -19,17 +19,16 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    # path('', include('authentication.urls')),
+    path('', RedirectView.as_view(url='/user/register', permanent=False)),
     path('property/', include('property.urls')),
     path('offer/', include('offer.urls')),
-
     path('user/', include('user_profile.urls')),
-
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG: # TODO EYÐA ÞESSU ÞEgar við skilum inn
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
