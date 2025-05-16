8bacdf8588bdd930cd1c0ffb13e5445993c45eba
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-05-16 11:36:08 +0000

    IMPORTANT EKKI EYÐA

--- castle_apartments/urls.py
+++ castle_apartments/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path("offer/", include("offer.urls")),
     path("user/", include("user_profile.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
